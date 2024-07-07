e15d83ae2cdef41de9e8a83c5ab835ee73b1a5b6
Ayush Srivastava <ayush.alp15@gmail.com>
2024-07-07 15:18:17 +0530

    Time: 24 ms (77.24%), Space: 30 MB (23.74%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+
+    int recur(int s, int e, vector<int> &nums, int target)
+    {
+        if (s>e) return -1;
+
+        int mid=(s+e)/2;
+        if (nums[mid]==target) return mid;
+        else if (nums[mid]<target) return recur(mid+1, e, nums, target);
+        else return recur(s, mid-1, nums, target);
+    }
+
     int search(vector<int>& nums, int target) {
         int s=0;
         int e=nums.size()-1;
-        while (s<=e)
-        {
-            int mid=(s+e)/2;
-            if (target==nums[mid]) return mid;
-            else if (target<nums[mid])
-            e=mid-1;
-            else
-            s=mid+1;
-        }
-
-        return -1;
+        
+        return recur(s, e, nums, target);
     }
 };
